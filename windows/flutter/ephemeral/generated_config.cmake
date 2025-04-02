cc9937232c52378fac5b67c0a5ffa015466dfa3d
triplesiete-777 <isaachdzr2007@gmail.com>
2025-04-02 09:15:40 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Usuario\\Documents\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Usuario\\Desktop\\mi primera app\\the_food" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Usuario\\Documents\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\Usuario\\Desktop\\mi primera app\\the_food"
+  "FLUTTER_ROOT=C:\\Users\\Usuario\\Documents\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Usuario\\Desktop\\mi primera app\\the_food\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Usuario\\Desktop\\mi primera app\\the_food"
+  "FLUTTER_TARGET=C:\\Users\\Usuario\\Desktop\\mi primera app\\the_food\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Usuario\\Desktop\\mi primera app\\the_food\\.dart_tool\\package_config.json"
+)
